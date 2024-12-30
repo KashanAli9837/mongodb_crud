a72beae6172a873435abe03d0d1be835ceefb65a
Kashan Ali <kashanali.2332@gmail.com>
2024-12-30 10:06:28 -0800

    changed again home

--- app/page.js
+++ app/page.js
@@ -1,24 +1,23 @@
 import TopicsList from "@/components/TopicsList";
 
 const Home = async () => {
-  // const data = await fetch(`${process.env.url}/api/topics`, {
-  //   cache: "no-store",
-  // });
+  const data = await fetch(`${process.env.url}/api/topics`, {
+    cache: "no-store",
+  });
 
-  // // Check if the response is okay
-  // if (!data.ok) {
-  //   return (
-  //     <div className="flex justify-center items-center flex-1">
-  //       <p className="text-lg font-semibold text-red-600">
-  //         Error fetching topics!
-  //       </p>
-  //     </div>
-  //   );
-  // }
+  // Check if the response is okay
+  if (!data.ok) {
+    return (
+      <div className="flex justify-center items-center flex-1">
+        <p className="text-lg font-semibold text-red-600">
+          Error fetching topics!
+        </p>
+      </div>
+    );
+  }
 
-  // const { topics } = await data.json();
-  // return <TopicsList topics={topics} />;
-  return "Hello"
+  const { topics } = await data.json();
+  return <TopicsList topics={topics} />;
 };
 
 export default Home;
